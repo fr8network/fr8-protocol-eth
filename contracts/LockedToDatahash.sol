3887441ad7b72d3a532bd4352662fb04f7eb102b
Yevgeniy Spektor <yevgeniy.spektor@gmail.com>
2018-07-23 13:25:12 -0700

    LockedToDatahash v0.0.1

--- contracts/LockedToDatahash.sol
+++ contracts/LockedToDatahash.sol
@@ -1,1 +1,25 @@
 pragma solidity ^0.4.24;
+
+/**
+ * @title LockedToDataHash v0.0.1
+ * @dev The LockedToDataHash contract stores a 256 bit hash of the off-chain object
+ * it represents.
+ */
+
+contract LockedToDataHash {
+  bytes32 public dataHash;
+
+  /**
+   * @dev Event emitted after any calls that modify permissions
+   */
+  event DatahashUpdated(address _sender, bytes32 _dataHash);
+
+  /**
+   * @dev Updates the data hash.
+   * @param _dataHash New hash.
+   */
+  function setDataHash(bytes32 _dataHash) public {
+    dataHash = _dataHash;
+    emit DatahashUpdated(msg.sender, _dataHash);
+  }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,3 @@
-var TestToken = artifacts.require('../contracts/TestToken.sol');
-var Balancer = artifacts.require('../contracts/Balancer.sol');
-
 module.exports = async function(deployer, network, accounts) {
-  await deployer.deploy(TestToken, 'TokenA', 'TKNA', 18, 10 ** 9 * 10 ** 18);
-  const tokenA = await TestToken.deployed();
-  await deployer.deploy(TestToken, 'TokenB', 'TKNB', 18, 10 ** 9 * 10 ** 18);
-  const tokenB = await TestToken.deployed();
 
-  // Balancer Constructor
-  // (basis, fee, slipbound, [token addresses], minBalance, live)
-  await deployer.deploy(Balancer, 10000, 10, 1000, [tokenA.address, tokenB.address], 1000000, true);
-  const balancer = await Balancer.deployed();
-
-  console.log('OWNER:', accounts[0]);
-  console.log('TOKEN A ADDRESS:', tokenA.address);
-  console.log('TOKEN B ADDRESS:', tokenB.address);
-  console.log('BALANCER ADDRESS:', balancer.address);
 };
